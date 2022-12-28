62abb69d9302adab83f6463565947d00637e1f73
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-12-28 17:28:11 +0100

    Add Contact List and filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { useAxios } from "./hooks/useAxios";
+import UserField from "./components/UserField";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+   const [filter, setFilter] = useState('');
+   const [loading, data, error] = useAxios({
+      method: 'GET',
+      url: 'https://teacode-recruitment-challenge.s3.eu-central-1.amazonaws.com/users.json'
+   });
 
-export default App;
+   if (loading) return <p>Loading ....</p>;
+   if (error !== '') return <p>{error}</p>;
+   if (!data) return <p>Data was null</p>;
+
+   const usersSortedAndFiltered = data?.sort((a, b) => (a.last_name > b.last_name) ? 1 : ((b.last_name > a.last_name) ? -1 : 0))
+      .filter(f => (f.first_name.concat(" ").concat(f.last_name)).toLowerCase().includes(filter.toLowerCase()) || filter === '');
+
+   return (
+      <div>
+         <div >
+            <label className="filter">
+               <span>Search:</span>
+               <input id="filter"
+                  name="filter"
+                  type="text"
+                  value={filter}
+                  onChange={event => setFilter(event.target.value)}
+               />
+            </label>
+         </div>
+         {usersSortedAndFiltered.map((user) => (
+            <UserField
+               key={user.id}
+               user={user} />
+         ))}
+      </div>
+   );
+}
